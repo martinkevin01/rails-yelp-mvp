1e1bcf9a690303f12bffe245ccc05891294cba4b
Martin Kevin Gapie <martinkevingapie@gmail.com>
2023-10-19 19:27:26 -0400

    refactoring coe

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/create'
-  get 'restaurants/new'
-  get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
